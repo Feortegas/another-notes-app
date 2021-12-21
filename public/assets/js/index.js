08bdae93c9397f941ab220d02c6a349faa1b5e48
Fernando Ortega <feortegas@gmail.com>
2021-12-21 16:30:33 -0600

    fix handle active note view

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -52,6 +52,7 @@ const deleteNote = (id) =>
 
 const renderActiveNote = () => {
   hide(saveNoteBtn);
+  console.log('Got to this point - rendeActiveNote');
 
   if (activeNote.id) {
     noteTitle.setAttribute('readonly', true);
